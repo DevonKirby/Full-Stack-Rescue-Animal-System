94878344c7bc459067529937302187e565d78cd6
Devon Kirby <devonlkirby@gmail.com>
2025-04-28 11:12:40 -0500

    Changed axios api creation to include a URL from the env

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Adjust the base URL as needed
+  baseURL: process.env.API_URL || 'http://localhost:5000/api',
 });
 
 api.interceptors.request.use((config) => {
